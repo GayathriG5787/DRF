179d52449f98555b129fbd02340c6bbbf5f3c92b
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-15 15:07:26 +0530

    comments views.py

--- api/views.py
+++ api/views.py
@@ -1,4 +1,7 @@
+# Converts Python dictionary into JSON
 from rest_framework.response import Response
+# Tells DRF that the fn is an API endpoint
+# handles DRF features like request parsing (converting JSON into python objects) and giving response
 from rest_framework.decorators import api_view
 
 @api_view(['GET'])
